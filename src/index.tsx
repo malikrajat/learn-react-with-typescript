63d44c49a56edd7f29e2ffce12493933321744d9
Rajat Malik <er.rajatmalik@gmail.com>
2020-10-10 09:45:34 +0530

    remove unused comment

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
-import { count } from "console";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/app";
 import Counter from "./app/counter";
+import CounterFC from "./app/CounterFC";
 
 const Root = () => {
 	return (
 		<>
 			<App color="red" />
 			<Counter />
+			<CounterFC color="blue" />
 		</>
 	);
 };
