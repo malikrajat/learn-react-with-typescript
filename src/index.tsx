c171f33c5cae3f737c5878540f491df55e8460a7
Rajat Malik <er.rajatmalik@gmail.com>
2020-10-10 14:56:20 +0530

    add store

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/app";
-import Counter from "./app/counter";
-import CounterFC from "./app/CounterFC";
-
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { reducers } from "./app/reducer";
+const store = createStore(reducers, applyMiddleware(thunk));
 const Root = () => {
 	return (
-		<>
+		<Provider store={store}>
 			<App color="red" />
-			<Counter />
-			<CounterFC color="blue" />
-		</>
+		</Provider>
 	);
 };
 ReactDOM.render(<Root />, document.querySelector("#root"));
