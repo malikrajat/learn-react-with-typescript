82e0bc277e64a0ec58e0aeea65c3c657cbee8f01
Rajat Malik <er.rajatmalik@gmail.com>
2020-10-10 21:31:04 +0530

    todo list

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Counter from "./counter";
 import CounterFC from "./CounterFC";
+import TodoList from "./TodoList";
 // if a component is expecting any kind of props from parent, that pros must have been defined in interface for class base component
 // if you want any props as options then put "?" in front of that key of interface
 interface AppProps {
@@ -11,6 +12,7 @@ export class app extends Component<AppProps> {
 	render() {
 		return (
 			<>
+				<TodoList />
 				<div>Hello from app {this.props.color}</div>
 				<Counter />
 				<CounterFC color="blue" />
